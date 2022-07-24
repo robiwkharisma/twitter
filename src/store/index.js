db13e36fd47a96e9394f0c6576fd501900eb3a93
Robi Wanda <robikharisma96@gmail.com>
2022-07-24 11:06:20 +0700

    Show tweet dinamically

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,17 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    feeds: [
+      {
+        tweet: "Keep smiling, because life is a beautiful thing and there's so much to smile about",
+        time: new Date()
+      }
+    ]
   },
   getters: {
+    feeds(state) {
+      return state.feeds
+    },
   },
   mutations: {
   },
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -74,7 +74,11 @@
               </div>
             </div>
           </b-card>
-          <div class="card p-0 text-left">
+          <div
+            v-for="(item, idx) in feeds"
+            :key="idx"
+            class="card p-0 text-left"
+            >
             <div class="card-header">
               <div class="d-flex">
                 <div class="px-2">
@@ -91,14 +95,14 @@
                     <h6 class="m-0"><strong>Robi W Kharisma</strong></h6>
                     <span style="font-size: 14px; color: #71767B">@robiwkharisma</span>
                     <span style="color: #71767B"> · </span>
-                    <span style="font-size: 14px; color: #71767B">18h</span>
+                    <span style="font-size: 14px; color: #71767B">{{item.time}}</span>
                   </div>
                 </div>
               </div>
             </div>
             <div class="card-body">
               <div class="px-2">
-                from “come over” to “come home” is a relationship goal for me
+                {{item.tweet}}
               </div>
             </div>
             <div class="card-footer">
@@ -107,19 +111,19 @@
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat" viewBox="0 0 16 16">
                     <path d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z"/>
                   </svg>
-                  <span class="px-2">10</span>
+                  <span class="px-2">{{Math.floor(Math.random() * 100)}}</span>
                 </div>
                 <div class="retweet"  style="cursor: pointer;">
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-up" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M11.5 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L11 2.707V14.5a.5.5 0 0 0 .5.5zm-7-14a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L4 13.293V1.5a.5.5 0 0 1 .5-.5z"/>
                   </svg>
-                  <span class="px-2">20</span>
+                  <span class="px-2">{{Math.floor(Math.random() * 100)}}</span>
                 </div>
                 <div class="love"  style="cursor: pointer;">
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
                     <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
                   </svg>
-                  <span class="px-2">30</span>
+                  <span class="px-2">{{Math.floor(Math.random() * 100)}}</span>
                 </div>
                 <div class="share"  style="cursor: pointer;">
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-share" viewBox="0 0 16 16">
@@ -137,12 +141,27 @@
 </template>
 
 <script>
-// @ is an alias to /src
-
+import { mapGetters } from 'vuex'
 export default {
   name: 'HomeView',
   components: {
   },
+  data() {
+    return {
+    }
+  },
+  computed: {
+    // mix the getters into computed with object spread operator
+    ...mapGetters([
+      'feeds',
+      // ...
+    ])
+  },
+  methods: {
+  },
+  mounted() {
+    console.log(this.$store.state);
+  }
 }
 </script>
 <style scoped>
